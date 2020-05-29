46d3f688990e29b73b115d80609fbfa6bb7f030a
Rafdapp <raf.dapp@gmail.com>
2020-05-29 21:02:51 +0100

    deploy todo component

--- rafs-todo-app/src/App.js
+++ rafs-todo-app/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Todo from './Todo';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="title">raf's todo list</h1>
+    <div className="app">
+      <h1 className="titleMain">raf's todo list</h1>
+
+      < Todo />    
+      < Todo />
+      < Todo />
+
+      <div className="todo">
+        <h2>this is todo</h2>
+        <p>my first task</p>
+      </div>
     </div>
   );
 }
---
+++ rafs-todo-app/src/Todo.js
@@ -0,0 +1,14 @@
+import React from'react';
+
+const Todo =() =>{           // es6 way to write  component
+
+    return (
+      <div className="todo">
+        <h2>this is todo</h2>
+        <p>my first task</p>
+      </div>
+    );
+};
+
+export default Todo;       // mean use this code in different file
+
